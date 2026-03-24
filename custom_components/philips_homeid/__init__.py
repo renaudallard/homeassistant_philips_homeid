e755eed09f61f689015f367da1c70e6b2be95cc9
Renaud Allard <renaud@allard.it>
2026-03-24 10:13:22 +0100

    Extend stale entity cleanup to cover number platform
    Add number entity descriptions (airfryer and MUJI) to the cleanup
    check so stale number entities are also removed when their backing
    properties disappear from the device.

--- custom_components/philips_homeid/__init__.py
+++ custom_components/philips_homeid/__init__.py
@@ -201,6 +201,17 @@ def _cleanup_stale_entities(
                         should_remove = True
                     break
 
+        elif platform == "number":
+            from .number import AIRFRYER_NUMBERS, MUJI_NUMBERS
+
+            for num_desc in (*AIRFRYER_NUMBERS, *MUJI_NUMBERS):
+                if num_desc.key == entity_key:
+                    if num_desc.property_key and not coordinator.has_property(
+                        num_desc.property_key, num_desc.nested_key
+                    ):
+                        should_remove = True
+                    break
+
         if should_remove:
             _LOGGER.info(
                 "Removing stale entity %s (%s)",
